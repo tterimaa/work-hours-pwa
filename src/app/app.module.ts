0d34936303b0d306fc9334cf0e956525fb8ca203
tterimaa <tterimaa@gmail.com>
2020-02-18 11:04:25 +0200

    Bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { EntryComponent } from './entry/entry.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { EntryComponent } from "./entry/entry.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EntryComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EntryComponent],
+  imports: [BrowserModule, FormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgbDateStruct, NgbCalendar } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-entry',
-  templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.scss']
+  selector: "app-entry",
+  templateUrl: "./entry.component.html",
+  styleUrls: ["./entry.component.scss"]
 })
 export class EntryComponent implements OnInit {
-  entry = {
-    hFrom: "",
-    hTo: "",
-  }
-  constructor() { }
+  model: NgbDateStruct;
+  time: {hour: 13, minute: 30}
+  constructor(private calendar: NgbCalendar) {}
 
   ngOnInit(): void {
+    this.model = this.calendar.getToday();
   }
-
 }
