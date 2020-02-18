dad366d2844865494a41169020a6ad8318213d8e
tterimaa <tterimaa@gmail.com>
2020-02-18 16:53:06 +0200

    Calculate day/evening hours

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from "@angular/core";
-import { NgbDateStruct, NgbCalendar } from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgbDateStruct,
+  NgbCalendar,
+  NgbTimeStruct
+} from "@ng-bootstrap/ng-bootstrap";
+
+interface Entry {
+  date: Date;
+  dayH: {
+    hours: number;
+    minutes: number;
+  };
+  eveningH: {
+    hours: number;
+    minutes: number;
+  };
+}
+
+const evening: NgbTimeStruct = {
+  hour: 18,
+  minute: 0,
+  second: 0
+};
+const timeLatest = 23;
+const timeEarliest = 6;
 
 @Component({
   selector: "app-entry",
@@ -7,11 +31,73 @@ import { NgbDateStruct, NgbCalendar } from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ["./entry.component.scss"]
 })
 export class EntryComponent implements OnInit {
-  model: NgbDateStruct;
-  time: {hour: 13, minute: 30}
+  date: NgbDateStruct;
+  timeFrom: NgbTimeStruct;
+  timeTo: NgbTimeStruct;
+  entry: Entry;
+  private isGreaterOrEqual = (a: NgbTimeStruct, b: NgbTimeStruct) => {
+    let bool;
+    if(a.hour - b.hour > 0) bool = true;
+    else if(a.hour - b.hour === 0 && a.minute - b.minute >= 0) bool = true;
+    else bool = false;
+    console.log(bool);
+    return bool;
+  }
+  private difference = (start: NgbTimeStruct, end: NgbTimeStruct) => {
+    let hours = end.hour - start.hour;
+    let minutes = end.minute - start.minute;
+    if(hours < 0) hours = 0;
+    if(minutes < 0) minutes = 0;
+    return { hours, minutes };
+  }
+  private calculateHours = () => {
+    // Evening
+    let eveningHours;
+    let dayHours;
+    if(this.isGreaterOrEqual(this.timeFrom, evening)) {
+      eveningHours = this.difference(this.timeFrom, this.timeTo);
+      dayHours = { hours: 0, minutes: 0}
+      return { dayHours, eveningHours }
+    }
+    if(this.isGreaterOrEqual(evening, this.timeTo)) {
+      dayHours = this.difference(this.timeFrom, this.timeTo)
+      eveningHours = { hours: 0, minutes: 0 }
+      return { dayHours, eveningHours }
+    }
+    dayHours = this.difference(this.timeFrom, evening);
+    eveningHours = this.difference(evening, this.timeTo);
+    return { dayHours, eveningHours }
+  }
+  private createEntry = (): Entry => {
+    if (!this.date || !this.timeFrom || !this.timeTo)
+      throw "Date, time from and time to must have a value";
+    if (this.timeFrom.hour < timeEarliest || this.timeFrom.hour > timeLatest)
+      throw `Time from must be between ${timeEarliest} and ${timeLatest}`;
+    if (this.timeTo.hour < timeEarliest || this.timeTo.hour > timeLatest)
+      throw `Time to must be between ${timeEarliest} and ${timeLatest}`;
+    const year = this.date.year;
+    const month = this.date.month;
+    const day = this.date.day;
+    const { dayHours, eveningHours } = this.calculateHours();
+    return {
+      date: new Date(year, month, day),
+      dayH: dayHours,
+      eveningH: eveningHours,
+    };
+  };
+  onSubmit(): void {
+    console.log("click");
+    let newEntry;
+    try {
+      newEntry = this.createEntry();
+    } catch (err) {
+      console.log(err);
+    }
+    this.entry = newEntry;
+  }
   constructor(private calendar: NgbCalendar) {}
 
   ngOnInit(): void {
-    this.model = this.calendar.getToday();
+    this.date = this.calendar.getToday();
   }
 }
